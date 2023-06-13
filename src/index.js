d380ef2b31d3e89cae1536d08c17c7c5ddb46b4d
Xavi Roca <xroca@vilamatica.com>
2023-06-13 08:47:49 +0200

    Iniciamos instalación REDUX

--- src/index.js
+++ src/index.js
@@ -13,6 +13,17 @@ if (accessToken) {
   setAuthorizationHeader(accessToken);
 }
 
+
+// quiero utilizar REDUX en mi proyecto
+
+// 1. instalar redux
+// 2. instalar react-redux
+// 3. crear el store  
+// 4. crear el reducer
+// 5. crear el action
+// 6. crear el componente
+// 7. conectar el componente con el store
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
